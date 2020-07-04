933e14ee306bdeacc60737d4b5589838f202ebae
liruiyi0 <214777227@qq.com>
2020-07-04 14:45:20 +0800

    first commit

---
+++ src/test/java/ruiyi/li/spring/test/TestSpring.java
@@ -0,0 +1,22 @@
+package ruiyi.li.spring.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import ruiyi.li.spring.service.UserServiceImpl;
+
+import java.lang.reflect.AnnotatedType;
+import java.security.acl.AclNotFoundException;
+
+public class TestSpring {
+    @Test
+    public void Test01(){
+        //创建核心容器，并且创建了对象
+        ApplicationContext act = new ClassPathXmlApplicationContext("spring.xml");
+       //从容器中获取对象
+        UserServiceImpl userServicel = (UserServiceImpl)act.getBean("UserServicel");
+
+    }
+}
